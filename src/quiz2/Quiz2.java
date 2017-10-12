e8c56d1437d7e0f198b34d18a25b73987b7df058
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-10-12 09:42:12 -0400

    Finished Quiz2 :)

--- src/quiz2/Quiz2.java
+++ src/quiz2/Quiz2.java
@@ -10,18 +10,18 @@ package quiz2;
  */
 public class Quiz2 {
 
-    //create a string methos to return the word in reverse mode
+    //create a String method to return the word in reverse mode
     public String reverseString(String word){
         
         //create a Base Case
-        //create an if statement that if  when the word length is 0, return it to the original word
+        //create an if statement that if  when the word length is 0
         if(word.length() == 0){
+            //return it to the original word
             return word;
             
         } else {
-            
-        //Call in again the method
-        //Return to the last letter and add it to the rest of the seperated word
+        //Else if it is not equal to zero   
+        //Call in again the method and return to the last letter and add it to the rest of the seperated word
         //And lastly print the word backwards
         return word.charAt(word.length() - 1) + reverseString(word.substring(0, word.length() - 1));
     }
@@ -33,7 +33,7 @@ public class Quiz2 {
         Quiz2 test = new Quiz2();
         
         //print out the reverse string
-        System.out.println(test.reverseString("cat"));
+        System.out.println(test.reverseString("tac"));
         System.out.println(test.reverseString("frog"));
         
 
